ea53e4bbc159d0180faa6dea13871a9c444b8099
Mubikwanaive <tshikombedzewanga@gmail.com>
2023-05-21 13:40:59 +0200

    Upload folder

---
+++ trial 2/network_graph2.py
@@ -0,0 +1,25 @@
+import csv
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+timestamps = []
+rtts = []
+
+with open('network_data100cm.csv', 'r') as file:
+    reader = csv.reader(file)
+    next(reader)  # Skip the header row
+    for row in reader:
+        if len(row) >= 2 and row[1]:  # Check if the row has at least two columns and the second column is not empty
+            try:
+                timestamp = float(row[0])
+                rtt = float(row[1])
+                timestamps.append(datetime.fromtimestamp(timestamp))
+                rtts.append(rtt)
+            except ValueError:
+                print(f"Skipping invalid row: {row}")
+#print(rtt)
+plt.plot(timestamps, rtts)
+plt.xlabel('Timestamp')
+plt.ylabel('Round-trip Time (ms)')
+plt.title('Network Stability at 100cm distance')
+plt.show()
